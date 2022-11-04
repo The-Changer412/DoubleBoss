6ffd6ece0cb14746302ebd3e5816f26c86395d63
The Changer <thechanger412@gmail.com>
2022-11-04 13:02:11 -0500

    Made all bosses that spawn in be double

--- DoubleBoss.cs
+++ DoubleBoss.cs
@@ -1,8 +1,36 @@
+using Terraria;
+using Terraria.DataStructures;
 using Terraria.ModLoader;
 
 namespace DoubleBoss
 {
 	public class DoubleBoss : Mod
 	{
+
 	}
+
+	public class ModifyNPCS : GlobalNPC
+	{
+        public override void OnSpawn(NPC npc, IEntitySource source)
+        {
+            if (npc.boss)
+            {
+                int counter = 0;
+                for (int i = 0; i < Main.npc.Length; i++)
+                {
+                    if (Main.npc[i].type == npc.type)
+                    {
+                        counter++;
+                    }
+                }
+
+                if (counter == 1)
+                {
+                    NPC.NewNPC(npc.GetSource_FromThis(), (int)npc.position.X, (int)npc.position.Y, npc.type);
+                    counter++;
+                }
+            }
+            base.OnSpawn(npc, source);
+        }
+    }
 }
