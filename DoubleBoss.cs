56da71913b9c8a0d0cc83112d8eb4613590f3e2d
The Changer <thechanger412@gmail.com>
2022-11-17 21:11:36 -0600

    updated the desc

--- DoubleBoss.cs
+++ DoubleBoss.cs
@@ -13,8 +13,10 @@ namespace DoubleBoss
 	{
         public override void OnSpawn(NPC npc, IEntitySource source)
         {
+            //check if npc is boss
             if (npc.boss)
             {
+                //count how many bosses are alive at the moment
                 int counter = 0;
                 for (int i = 0; i < Main.npc.Length; i++)
                 {
@@ -24,6 +26,7 @@ namespace DoubleBoss
                     }
                 }
 
+                //if there is only one of that boss, then spawn it in
                 if (counter == 1)
                 {
                     NPC.NewNPC(npc.GetSource_FromThis(), (int)npc.position.X, (int)npc.position.Y, npc.type);
